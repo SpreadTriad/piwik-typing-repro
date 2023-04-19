a81eddbe73f9091cf20edd66323c856cab23caca
rogier83 <rogier@oberon.nl>
2023-04-19 17:28:31 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import PiwikProSdk, { PiwikProSdkType } from '@piwikpro/react-native-piwik-pro-sdk'
+
+const a = () => {
+    PiwikProSdk.initialize('https://demo.piwik.pro', 1, 'a1b2c3d4e5f6g7h8i9j0')
+}
+
+export default a
